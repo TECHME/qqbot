a0f67acb66e43d251eca93ecc6ca4c587ca6fc8d
InvXp <invidentssc@hotmail.com>
2012-12-20 16:41:25 +0800

    this is a simple irc client

--- libirc/irc.cpp
+++ libirc/irc.cpp
@@ -1,555 +1,169 @@
-/* $ID$ */
 #include "irc.h"
-#include <assert.h>
-#include <iostream>
-#ifndef WIN32
-#include <arpa/inet.h>
-#endif
-#include <stdarg.h>
-#include <string>
-#include <errno.h>
 
-/********************************************************************/
-
-Irc::Irc(privmsg_cb callback)
-	: crlf("\r\n"), buffer_index(0),cb(callback)
+IrcClient::IrcClient(const privmsg_cb &cb,const std::string& server, const std::string& port):cb_(cb),resolver_(io_service_),socket_(io_service_)
 {
-#ifdef WIN32
-    WSAData dd;
-    WSAStartup(MAKEWORD(2,2),&dd);
-#endif
+    boost::asio::ip::tcp::resolver::query query(server,port);
+    boost::system::error_code ec;
+    boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver_.resolve(query,ec);
 
-	buffer = new char[512+1];
-	memset(buffer, 0, 512+1);
-	
-	addr = reinterpret_cast<sockaddr_in*> (new sockaddr_in) ;
-	addr->sin_family = AF_INET;
-    sock = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-}
-
-/********************************************************************/
-
-Irc::~Irc() {
-	disconnect();
-	delete [] addr;
-	delete [] buffer;
-}
-
-/********************************************************************/
-
-void
-Irc::sendMessage(const string cmd, const char *param, ...) const {
-	string msg = cmd;
-	
-	va_list arg;
-	
-	if(param)	va_start(arg, param);
-	while(param) {
-		string tmp = param;
-		param = va_arg(arg, char *);
-		if(!param)	msg += " :";
-		else msg += " ";
-		msg += tmp;
-	}
-	va_end(arg);
-
-	msg += crlf;
-#ifdef DEBUG
-	cout << msg;
-#endif
-	send(sock, msg.c_str(), msg.length(), 0);	
-}
-
-
-/********************************************************************/
-
-string
-Irc::host2ip(const char* host)
-{
-    hostent* lpHostEnt=gethostbyname(host);
-    if(lpHostEnt==0)
-        return "";
-
-    char* lpAddr = lpHostEnt->h_addr_list[0];
-    if(lpAddr)
+    if (!ec)
     {
-        struct in_addr inAddr;
-        inAddr.s_addr = *(u_long *)lpAddr;
-        char* p=inet_ntoa(inAddr);
-        if(!p)
-            return "";
-        else
-            return (std::string)p;
+        boost::asio::async_connect(socket_, endpoint_iterator,
+            boost::bind(&IrcClient::handle_connect, this,
+            boost::asio::placeholders::error));
+        
+        boost::thread td(boost::bind(&boost::asio::io_service::run,&io_service_));
     }else
-        return "";
-
-}
-
-void
-Irc::connect(const string host, unsigned short port) {
-
-	// first, look up hostname
-    string ip=host2ip(host.c_str());
-
-	struct sockaddr_in *in = reinterpret_cast<sockaddr_in*>(addr);
-    in->sin_addr.s_addr= inet_addr(ip.c_str());
-	in->sin_port = htons(port);
-    in->sin_family = AF_INET;
-
-    int ret = ::connect(sock, (sockaddr*)addr, sizeof(sockaddr_in));
-	if (ret == -1)		
-        perror("connect");
-}
-
-/********************************************************************/
-
-void
-Irc::disconnect() {
-	shutdown(sock,2);
-}
-
-/********************************************************************/
-
-bool Irc::findCTCP(string msg, string &ctcp) {
-	size_t start, end;
-	
-	start = msg.find('\001');
-	if(start == string::npos) return false;
-	end = msg.find('\001', start+1);
-	if(end == string::npos) return false;
-	ctcp = msg.substr(start+1, end-start-1);
-	return true;
-}
-
-/********************************************************************/
-
-void
-Irc::processMessage(IrcMessage m) {
-	// PING
-	if(m.command == "PING" && m.pcount == 1)
-        this->pong(m.param[0]);
-
-	if(m.have_prefix) {
-		// JOIN
-		if(m.command == "JOIN" && m.pcount == 1);
-			//onJoin(this, m.prefix, m.param[0]);
-		
-		// PRIVMSG and CTCP
-		if(m.command == "PRIVMSG" && m.pcount == 2) {
-			string ctcp;
-			if(findCTCP(m.param[1], ctcp)) {
-				// this is a CTCP data string
-				// TODO: embedded CTCP?
-				//if(ctcp == "VERSION") onCTCPVersion(this,m.prefix);
-				//if(ctcp == "FINGER") onCTCPFinger(this,m.prefix);
-				//if(ctcp == "SOURCE") onCTCPSource(this,m.prefix);
-				//if(ctcp == "USERINFO") onCTCPUserInfo(this,m.prefix);
-				//if(ctcp == "CLIENTINFO") onCTCPClientInfo(this,m.prefix);
-				//if(ctcp == "PING") onCTCPPing(this,m.prefix);
-			} else {
-				// normal PRIVMSG
-				//onPrivMsg(this, m.prefix, m.param[0], m.param[1]);
-                cb(m);
-                //cout << m.prefix + "::" + m.param[0]+"||"+m.param[1] <<endl;
-			}
-		} else if(m.command == "PRIVMSG" && m.pcount != 2) {
-			cout << "TODO: privmsg with pcount != 2" << endl;
-		}
-	}
-		
-	// RPL_WELCOME
-	if(m.command == "001") {
-		string welcome_msg;
-		for(size_t i = 0; i < m.pcount; i++) {
-			welcome_msg += " ";
-			welcome_msg += m.param[i];
-		}
-		//onRegistered(this, welcome_msg);
-	}
-}
-
-/********************************************************************/
-
-Irc::IrcMessage
-Irc::parseMessage(const string msg) {
-
-	IrcMessage m;
-	for(int i = 0; i < 15; i++)
-		m.param[i] = "";
-	m.command = "";
-	m.prefix = "";
-	
-	m.have_prefix = (msg[0] == ':');
-	
-	// extract prefix from message if present
-	size_t msgstart = 0;
-	if(m.have_prefix) {
-		size_t pos = msg.find(" ");
-		m.prefix = msg.substr(1, pos - 1);
+    {
 #ifdef DEBUG
-		cout << "[prefix] " << m.prefix << endl;
+        std::cout << "Error: " << ec.message() << "\n";
 #endif
-		msgstart = pos+1;
-	}
+    }
 
-	// extract irc command
-	size_t paramstart = msg.find(" ", msgstart) + 1;
-	m.command = msg.substr(msgstart, paramstart - 1 - msgstart);
-#ifdef DEBUG
-	cout << "[cmd] " << m.command << endl;
-#endif
-
-	m.pcount = 0;
-	while(m.pcount < 15) {
-		// first check for ':' param prefix
-		// this means that this is the last parameter
-		if((msg[paramstart] == ':')) {
-			m.param[m.pcount] = msg.substr(
-					paramstart+1,msg.length()-1-paramstart);
-			m.pcount++;
-			break;
-		}
-		// try to localise next parameter
-		// if not present ... we know this is the last
-		size_t next = next = msg.find(" ", paramstart);
-		if((next == string::npos)) {
-			m.param[m.pcount] = msg.substr(
-					paramstart,
-					msg.length() - paramstart
-				);
-			m.pcount++;
-			break;
-		}
-		// there are more than this parameter so
-		// set paramstart to the next parameter
-		m.param[m.pcount] = msg.substr(paramstart, next-paramstart);
-		paramstart = next+1;
-		m.pcount++;
-	}
-#ifdef DEBUG
-	cout << "[pcount] " << m.pcount << endl;
-	for(size_t i = 0; i < m.pcount; i++)
-		cout << "[param][" << i << "] " << m.param[i] << endl;
-#endif
-
-	return m;
 }
 
-/********************************************************************/
-
-string
-Irc::nextMessage() {
-	string tmp = buffer;
-	size_t start = 0;	
-	size_t end = tmp.find(crlf);
-	
-	/* first, check if we already have an IRC message in the buffer */
-	if(end != string::npos) {
-		// extract the message
-		string msg = tmp.substr(start,end-start);
-		// set start to point 2 bytes after the message
-		// we just extracted
-		start = end + 2; 
-		
-		// now check whether the buffer was holding more 
-		// data than that message extracted
-		if(start < tmp.length()) { // yes
-			// calculate new size of buffer where the 
-			// message extracted is but out
-			size_t newsize = tmp.length() - start;
-			// create a temporary string that holds 
-			// the contents of the new buffer
-			string newbuf = tmp.substr(start,newsize);
-			// copy the contents of the new buffer to
-			// the buffer variable and reset remaining
-			// characters to 0 bytes
-			strncpy(buffer, newbuf.c_str(), newsize);
-			memset(buffer+newsize, 0, 512-newsize+1);
-			cout << "buffer = '" << buffer << "'" << endl;
-			// update the buffer index to reflect the
-			// changes
-			buffer_index = static_cast<int>(newsize);
-		} else { // no
-			// update buffer index to point to beginning
-			// of buffer
-			buffer_index = 0;
-			// and reset the contents of the buffer
-			// to 0 bytes
-			memset(buffer, 0x0, 512+1);
-		}
-		// return the message extracted
-		return msg;
-	}
-	
-	int read = 0;  // used to test whether we are at EOF (socket closed)
-	while((buffer_index += read = 
-				recv(sock, buffer+buffer_index, 512-buffer_index, 0)) != -1) {
-		tmp = buffer;
-		start = 0;
-		end = tmp.find(crlf);
-		
-		/* check if we have an IRC message in the buffer */
-		if(end != string::npos) {
-			// extract the message
-			string msg = tmp.substr(start,end-start);
-			// set start to point 2 bytes after the message
-			// we just extracted
-			start = end + 2; 
-			
-			// now check whether the buffer was holding more 
-			// data than that message extracted
-			if(start < tmp.length()) { // yes
-				// calculate new size of buffer where the 
-				// message extracted is but out
-				size_t newsize = tmp.length() - start;
-				// create a temporary string that holds 
-				// the contents of the new buffer
-				string newbuf = tmp.substr(start,newsize);
-				// copy the contents of the new buffer to
-				// the buffer variable and reset remaining
-				// characters to 0 bytes
-				strncpy(buffer, newbuf.c_str(), newsize);
-				memset(buffer+newsize, 0, 512-newsize+1);
-				cout << "buffer = '" << buffer << "'" << endl;
-				// update the buffer index to reflect the
-				// changes
-				buffer_index = static_cast<int>(newsize);
-			} else { // no
-				// update buffer index to point to beginning
-				// of buffer
-				buffer_index = 0;
-				// and reset the contents of the buffer
-				// to '\0' bytes
-				memset(buffer, 0x0, 512+1);
-			}
-			// return the message extracted
-			return msg;
-		}
-		/* if the socket was closed read will be 0 (indicates EOF) */
-		if(read == 0) throw ConnectionClosedException();
-	} // while
-	// TODO error handling
-	// the line below will cause segmentation fault
-	return NULL;
-}
-
-/********************************************************************/
-void
-Irc::eventLoop() {
-	while(1) {
-		try {
-			// read next IRC message from server
-			string msg = nextMessage();
-			#ifdef DEBUG
-			cout << "[msg] " << msg << endl;
-			#endif 
-			// parse the message into a IrcMessage struct
-			IrcMessage m = parseMessage(msg);
-			// examine/process the message and 
-			// emit appropriate signals
-			processMessage(m);
-		} catch (ConnectionClosedException) {
-			throw ConnectionClosedException();
-		}
-	}
-}
-
-/********************************************************************/
-
-string Irc::loQuote(const string msg) {
-	const string q("\0\r\n\020");
-
-	// TODO optimize (important?)
-	
-	string tmp("");
-	for(size_t i = 0; i < msg.length(); i++) {
-		if(q.find(msg[i]) != string::npos)
-			tmp += '\020';
-		tmp += msg[i];
-	}
-	return tmp;
-}
-
-/********************************************************************/
-
-bool 
-Irc::loUnQuote(const string msg, string &result) {
-	size_t last = 0;
-	size_t f = 0;
-	
-	result = "";
-	while((f = msg.find('\020', last)) != string::npos) {
-		switch(msg[f+1]) {
-			case '\0':
-			case '\r':
-			case '\n':
-			case '\020': {
-				result += msg.substr(last, f - last);
-				result += msg[f+1];
-				last = f+2;
-				break;
-			}
-			default:
-				return false;
-		}
-	}
-	result += msg.substr(last, msg.length() - last + 1);
-}
-
-/********************************************************************/
-
-void Irc::notice(const string whom, const string msg) {
-	sendMessage("NOTICE", whom.c_str(), msg.c_str(), 0);
-}
-
-/********************************************************************/
-
-void 
-Irc::nick(const string nick) {
-	sendMessage("NICK", nick.c_str(), 0);
-}
-
-/********************************************************************/
-
-void 
-Irc::pass(const string password) {
-	sendMessage("PASS", password.c_str(), 0);
-}
-
-/********************************************************************/
-
-void 
-Irc::user(const string user, const string mode, const string realname) {
-	sendMessage("USER", user.c_str(), mode.c_str(), "*", realname.c_str(), 0);
-}
-
-/********************************************************************/
-void 
-Irc::oper(const string name, const string password) {
-	sendMessage("OPER", name.c_str(), password.c_str(), 0);
-}
-
-/********************************************************************/
-
-void 
-Irc::userMode(const string nick, const string modestring) {
-	sendMessage("MODE", nick.c_str(), modestring.c_str(), 0);
-}
-
-/********************************************************************/
-
-void 
-Irc::quit(const string message) {
-	if(message != "")	sendMessage("QUIT", message.c_str(), 0);
-	else sendMessage("QUIT", 0);
-}
-
-/********************************************************************/
-
-void 
-Irc::join(const string channel, const string key) {
-	if(key != "") sendMessage("JOIN", channel.c_str(), key.c_str(), 0);
-	else sendMessage("JOIN", channel.c_str(), 0);
-}
-
-/********************************************************************/
-
-void 
-Irc::pong(const string id) {
-	sendMessage("PONG", id.c_str(), 0);
-}
-
-/********************************************************************/
-
-void 
-Irc::privmsg(const string whom, const string msg) {
-	sendMessage("PRIVMSG", whom.c_str(), msg.c_str(), 0);
-}
-
-/********************************************************************/
-
-void 
-Irc::channelOp(const string channel, const string nick, bool op) {
-	sendMessage("MODE", channel.c_str(), (op ? "+o" : "-o"), 
-			nick.c_str(), 0);	
-}
-
-/********************************************************************/
-bool
-Irc::extractFromHostmask(const string whom, string &nick, 
-				string &user, string &host) 
+IrcClient::~IrcClient()
 {
-	// a hostmask is composed like
-	// nick!username@hostname
-	size_t sep1 = whom.find("!");
-	if(sep1 == string::npos) return false;
-	size_t sep2 = whom.find("@", sep1+1);
-	if(sep2 == string::npos) return false;
 
-	nick = whom.substr(0, sep1);
-	user = whom.substr(sep1+1, sep2-sep1-1);
-	host = whom.substr(sep2+1, whom.length()-sep2);
-
-	return true;
 }
 
-/********************************************************************/
-
-Irc::IrcMessage
-Irc::waitForMessage(const string command) {
-	// maybe put a threshold here .. like give
-	// up when read 100 messages ?
-	while(1) {
-		string msg = nextMessage();
-		#ifdef DEBUG
-		cout << "[msg] " << msg << endl;
-		#endif 
-		IrcMessage m = parseMessage(msg);
-		if(command.find(m.command)) return m;
-		else processMessage(m);
-	}
+void IrcClient::chat(const std::string& whom,const std::string& msg)
+{
+    send_request("PRIVMSG "+whom+" :"+msg);
 }
 
-/********************************************************************/
-
-void 
-Irc::topic(const string channel, const string topic) {
-	sendMessage("TOPIC", channel.c_str(), topic.c_str(), NULL);
+void IrcClient::login(const std::string& user,const std::string& ch)
+{
+    send_request("NICK "+user);
+    send_request("USER "+user+ " 0 * "+user);
+    send_request("JOIN "+ch);
 }
 
-/********************************************************************/
+void IrcClient::process_request()
+{
+    std::string req;
 
-string 
-Irc::getChannelTopic(const string channel) {
-/*
-	331    RPL_NOTOPIC "<channel> :No topic is set"
-	332    RPL_TOPIC "<channel> :<topic>"
-	442    ERR_NOTONCHANNEL "<channel> :You're not on that channel
-	482    ERR_CHANOPRIVSNEEDED "<channel> :reason"
-	477    ERR_NOCHANMODES "<channel> :Channel doesn't support modes"
-*/
-	sendMessage("TOPIC", channel.c_str(), 0);
-	IrcMessage m = waitForMessage("331 332 442 482 477");
-	if(m.param[1] == channel) {
-		string topic( m.param[2]); 
-		if(m.command == "332")	return topic;
-		if(m.command == "331") return "";
-		if(m.command == "442") throw NotOnChannelException();
-		if(m.command == "482") throw ChanopPrivsNeededException();
-		if(m.command == "477") throw NoChanModesException();
-	}
-	processMessage(m);
-	return "";
+    std::istream is(&response_);
+
+    is.unsetf(std::ios_base::skipws);
+
+    req.append(std::istream_iterator<char>(is), std::istream_iterator<char>());
+
+#ifdef DEBUG
+    std::cout << req;
+#endif
+
+    if (req.substr(0,4)=="PING")
+        send_request("PONG "+req.substr(6,req.length()-8));
+    
+    size_t pos=req.find(" PRIVMSG ")+1;
+
+    if (pos)
+    {
+        std::string msg=req;
+        IrcMsg m;
+
+        pos=msg.find("!")+1;
+        if (!pos)
+            return;
+
+        m.whom=msg.substr(1,pos-2);
+
+        msg=msg.substr(pos);
+
+        pos=msg.find(" PRIVMSG ")+1;
+        if (!pos)
+            return;
+
+        m.locate=msg.substr(0,pos-1);
+
+        msg=msg.substr(pos);
+
+        pos=msg.find("PRIVMSG ")+1;
+
+        if (!pos)
+            return;
+
+        msg=msg.substr(strlen("PRIVMSG "));
+
+        pos=msg.find(" ")+1;
+
+        if (!pos)
+            return;
+        
+        m.from=msg.substr(0,pos-1);
+
+        msg=msg.substr(pos);
+
+        pos=msg.find(":")+1;
+
+        if (!pos)
+            return;
+
+        m.msg=msg.substr(pos,msg.length()-2-pos);
+
+        cb_(m);
+    }
+
 }
 
-/********************************************************************/
-
-void Irc::part(const string channel, const string reason) {
-	if(reason != "")
-		sendMessage("PART", channel.c_str(), reason.c_str(), 0);
-	else
-		sendMessage("PART", channel.c_str(), 0);
+void IrcClient::handle_connect(const boost::system::error_code& err)
+{
+    if (!err)
+    {
+        boost::asio::async_write(socket_, request_,
+            boost::bind(&IrcClient::handle_write_request, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+#ifdef DEBUG
+        std::cout << "Error: " << err.message() << "\n";
+#endif
+    }
 }
 
-/********************************************************************/
+void IrcClient::handle_write_request(const boost::system::error_code& err)
+{
+    if (!err)
+    {
 
+        boost::asio::async_read_until(socket_, response_, "\n",
+            boost::bind(&IrcClient::handle_read_request, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+#ifdef DEBUG
+        std::cout << "Error: " << err.message() << "\n";
+#endif
+    }
+}
+
+void IrcClient::handle_read_request(const boost::system::error_code& err)
+{
+    if (!err)
+    {
+        process_request();
+
+        boost::asio::async_read_until(socket_, response_, "\n",
+            boost::bind(&IrcClient::handle_read_request, this,
+            boost::asio::placeholders::error));
+    }
+    else if (err != boost::asio::error::eof)
+    {
+#ifdef DEBUG
+        std::cout << "Error: " << err.message() << "\n";
+#endif
+    }
+}
+
+void IrcClient::send_request(const std::string& msg)
+{
+    std::ostream request_stream(&request_);
+    request_stream << msg+"\r\n";
+
+    boost::asio::async_write(socket_, request_,
+        boost::bind(&IrcClient::handle_read_request, this,
+        boost::asio::placeholders::error));
+}
--- libirc/irc.h
+++ libirc/irc.h
@@ -1,163 +1,40 @@
-/* $ID$ */
-#ifndef _IRC_H
-#define _IRC_H
+#pragma once
 
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread/thread.hpp>
 #include <boost/function.hpp>
 
-#ifndef WIN32
-#include <sys/socket.h>
-#include <netdb.h>
-#else
-#include <WinSock.h>
-#endif
-#include <stdio.h>
-#include <string.h>
-#include <string>
-
-using namespace std;
-
-
-class Irc {
-	private:
-		struct sockaddr_in *addr;
-		int sock;
-		string crlf;
-		char *buffer;
-		int buffer_index;
-    public:
-		struct IrcMessage {
-			bool have_prefix;
-			string prefix;
-			string command;
-			string param[15];
-			size_t pcount;
-		};
-        typedef boost::function<void(const IrcMessage & pMsg)> privmsg_cb;
-        privmsg_cb cb;
-
-	protected:
-		struct hostent *host;
-		void sendMessage(const string cmd, const char *param, ...) const;
-		IrcMessage parseMessage(const string msg);
-		IrcMessage waitForMessage(const string command);
-		void processMessage(IrcMessage m);
-		/** finds CTCP data in msg
-		 * \param msg second parameter from a privmsg
-		 * \param ctcp will hold the CTCP data in string if any
-		 * \return true if CTCP data was present */
-		bool findCTCP(string msg, string &ctcp);
-		/** ctcp low-level quoting of a message */
-		string loQuote(const string msg);
-		/** ctcp low-lewel un-quoting of a message\
-		 * \param msg message to unquote
-		 * \param result reference to string to hold the unqouted result
-		 * \return true if no errors accured, false otherwise (message should
-		 * be dropped then) */
-		bool loUnQuote(const string msg, string &result);
-
-		/** reads and blocks until the next IRC message can be read
-		 * from the current connection
-		 * \throws ConnectionClosed
-		 * \return IRC message read <b>not</b> including linefeed/newline */
-		string nextMessage();
-		
-        //add by invxp
-        string host2ip(const char* host);
-	public:
-		Irc(privmsg_cb cb);
-		~Irc();
-
-		/** attempts to establish a connection to an IRC server
-		 * \param host hostname or ip of the server to connect to
-		 * \param port port number of server */
-		void connect(const string host, unsigned short port = 6667);
-
-		/** disconnects from IRC server */
-		void disconnect();
-		
-		void eventLoop();
-	
-		/** changes nickname to nick */
-		void nick(const string nk);
-		/** set connection password */
-		void pass(const string password);
-		/** 
-		 * The USER command is used at the beginning of connection 
-		 * to specify the username, hostname and realname of a new 
-		 * user. 
-		 * \param user username
-		 * \param mode automatically set mode on connection (bitmask)
-		 * \param realname real name, can contain spacesb */
-		void user(const string u, const string mode, const string realname);
-		
-		/** used by normal users to gain IRC operator status */
-		void oper(const string name, const string password);
-
-		void userMode(const string nick, const string modestring);
-
-		/** terminate client session with server 
-		 * \param message optional message/reason to send the IRC network */
-		void quit(const string message = "");
-
-		/** starts listening to channed.
-		 * \param channel name of channel to listen to
-		 * \param key key/password to join channel (optional) */
-		void join(const string channel, const string key = "");
-
-		/** respond to PING
-		 * \param id parameter received from PING command */
-		void pong(const string id);
-
-		/** sends message to whom
-		 * \param whom irc nickname / channelname
-		 * \param msg message to send */
-		void privmsg(const string whom, const string msg);
-	
-		/** sends notice to whom
-		 * \param whom receiver of notice
-		 * \param msg message to send */
-		void notice(const string whom, const string msg);
-		
-		/** split irc hostmask into nickname, username and
-		 * hostname
-		 * \param whom string to parse
-		 * \param nick string to receive nickname
-		 * \param user string to receive username
-		 * \param host string to receive hostname
-		 * \return true on succes, false otherwise */
-		static bool extractFromHostmask(const string whom, string &nick, 
-				string &user, string &host);
-	
-		/** gives/takes channel operator status
-		 * \param channel channel to give/take op
-		 * \param nick irc nickname to give op
-		 * \param give true = gives op; false = takes op */
-		void channelOp(const string channel, const string nick, bool give);
-
-		/** sets topic of an irc channel
-		 * \param channel channel subject to change
-		 * \param topic new topic string */
-		void topic(const string channel, const string top);
-
-		/** queries IRC server for current topic on channel
-		 * \throws NotOnChannelException
-		 * \throws ChanopPrivsNeededException
-		 * \throws NoChanModesException
-		 * \return topic for channel */
-		string getChannelTopic(const string channel) ;
-
-		/** parts a channel
-		 * \param channel channel to leave
-		 * \param reason optional reason for leaving */
-		void part(const string channel, const string reason = "");
-
-		/* ---- exceptions ---- */
-		class IrcException { };
-		class UnknownHostException : public IrcException { };
-		class NotOnChannelException : public IrcException { };
-		class ChanopPrivsNeededException : public IrcException { };
-		class NoChanModesException : public IrcException { };
-		class ConnectionClosedException : public IrcException { };
+struct IrcMsg
+{
+    std::string whom;
+    std::string from;
+    std::string locate;
+    std::string msg;
 };
 
-#endif
+typedef boost::function<void(const IrcMsg& msg)> privmsg_cb;
+
+class IrcClient
+{
+public:
+    IrcClient(const privmsg_cb &cb,const std::string& server, const std::string& host);
+    ~IrcClient();
+private:
+    void handle_connect(const boost::system::error_code& err);
+    void handle_write_request(const boost::system::error_code& err);
+    void handle_read_request(const boost::system::error_code& err);
+    void send_request(const std::string& msg);
+    void process_request();
+public:
+    void login(const std::string& user,const std::string& ch);
+    void chat(const std::string& whom,const std::string& msg);
+private:
+    boost::asio::io_service         io_service_;
+    boost::asio::ip::tcp::resolver  resolver_;
+    boost::asio::ip::tcp::socket    socket_;
+    boost::asio::streambuf          request_;
+    boost::asio::streambuf          response_;
+    privmsg_cb                      cb_;
+
+};
